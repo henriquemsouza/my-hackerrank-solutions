d18838f970b8ff30c8b3a82a4e4c67f6f129ed44
henriquemsouza <henrique.m_s@hotmail.com>
2021-10-16 11:18:51 -0300

    chore: add more js solutions

---
+++ Problem Solving/mini-max-sum.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+let sumValue = arr.reduce((a, b) => {
+  return a + b;
+});
+
+const min = sumValue - Math.max(...arr);
+const max = sumValue - Math.min(...arr);
+const result =  `${min} ${max}`
+
+console.log(result);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
